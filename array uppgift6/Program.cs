6343f36d21db61ec24f14776706a9bcdc92820ea
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:13:44 +0200

    Add project files.

---
+++ array uppgift6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ttal separerade med det här teckmet *:");
+            string inmatning = Console.ReadLine();
+            string[] tal = inmatning.Split('*');
+            int produkt = 1;
+            foreach (string s in tal)
+            {
+                produkt *= int.Parse(s);
+            }
+            Console.WriteLine(produkt);
+        }
+    }
+}
